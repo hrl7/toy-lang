9ea817fea2a8a9a5fcf43f67ced8f9a1f8dae30c
zigen <zigen@adawarp.com>
2019-04-06 18:54:26 +0900

    parse multi-digit number

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,16 +1,29 @@
 const { TK_TYPES, TK_CONST_KEYS, TK_CONSTS } = require("./constants");
 const tokenize = src => {
   const createNumber = n => ({ type: TK_TYPES.NUMBER, value: parseInt(n) });
+  const isNum = code => code >= 48 && code <= 57;
 
   let i = 0;
   const tokens = [];
 
   while (i < src.length) {
-    const c = src[i];
-    const charCode = src.charCodeAt(i);
-    if (charCode >= 48 && charCode <= 57) {
-      tokens.push(createNumber(c));
+    let c = src[i],
+      charCode = src.charCodeAt(i);
+    if (/\s/.test(c)) {
       i++;
+      continue;
+    }
+
+    if (isNum(charCode)) {
+      let buf = "";
+      while (isNum(charCode)) {
+        buf += c;
+        i++;
+        charCode = src.charCodeAt(i);
+        c = src[i];
+      }
+
+      tokens.push(createNumber(buf));
       continue;
     }
 
